471a97971746fc3f2677b7111b8af821760d0349
Francesco <todinofrancesco97@gmail.com>
2023-12-12 23:36:23 +0100

    splitted init with approx in main

--- src/main/java/org/oristool/qesm/Main.java
+++ src/main/java/org/oristool/qesm/Main.java
@@ -14,30 +14,33 @@ public class Main {
       stations.add(new GatedStation());
 
       int nStationCompleted = 0;
+      // INIT STATIONS
+      for (int i = 0; i < stations.size(); i++) {
+         Station station = stations.get(i);
 
-      for (int j = 0; j < 100; j++) {
+         double[] newTimes = station.exec();
+         station.setTimes(newTimes);
+      }
+
+      // EXECUTE APPROXIMATION
+      while (true) {
          nStationCompleted = 0;
          for (int i = 0; i < stations.size(); i++) {
             Station station = stations.get(i);
 
-            if (station.getTimes() == null) {
-               double[] newTimes = station.exec();
-               station.setTimes(newTimes);
-            } else {
-               List<double[]> otherTimes = new ArrayList<double[]>();
-               for (int k = 0; k < stations.size(); k++) {
-                  if (k != i) {
-                     otherTimes.add(stations.get(k).getTimes());
-                  }
+            List<double[]> otherTimes = new ArrayList<double[]>();
+            for (int k = 0; k < stations.size(); k++) {
+               if (k != i) {
+                  otherTimes.add(stations.get(k).getTimes());
                }
-               station.updatePNWithApproxTimes(otherTimes);
+            }
+            station.updatePNWithApproxTimes(otherTimes);
 
-               double[] newTimes = station.exec();
-               if (station.isTimesDiffInThreshold(newTimes, 10E-9)) {
-                  nStationCompleted++;
-               } else {
-                  station.setTimes(newTimes);
-               }
+            double[] newTimes = station.exec();
+            if (station.isTimesDiffInThreshold(newTimes, 10E-9)) {
+               nStationCompleted++;
+            } else {
+               station.setTimes(newTimes);
             }
          }
 
